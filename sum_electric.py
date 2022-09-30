078d5f99f9f63c5a894dc3754b13d11f9320eaa1
angelo562 <username@example.com>
2022-09-30 12:43:24 -0700

    master->main, reformatted file

--- sum_electric.py
+++ sum_electric.py
@@ -9,6 +9,7 @@
 import pandas as pd
 import sys
 
+
 def main():
     p = sys.argv[1]
     # p = "C:\cs_files\Python\csv\electric.csv"
@@ -16,27 +17,27 @@ def main():
     write_to_excel(g)
 
 
-def groupby_sum_kwh(p:str) -> dict:
+def groupby_sum_kwh(p: str) -> dict:
     """ Read CSV file, sums kwH by month.  Returns -> dict"""
     try:
         df = pd.read_csv(p)
         # reads Startdate and changes from type obj -> datetime64
-        df.Startdate = pd.to_datetime(df.Startdate, format='%Y/%m/%d') 
+        df.Startdate = pd.to_datetime(df.Startdate, format='%Y/%m/%d')
         df.index = df.Startdate
         # groupby, t is a zip object
-        t = df.groupby(pd.Grouper(freq='M')).Usage.sum() 
+        t = df.groupby(pd.Grouper(freq='M')).Usage.sum()
         # k is a timestamp object. Set a dictionary to str of month and KwH sum
-        d = {str(k)[:7]:round(v) for k, v in t.items()}
+        d = {str(k)[:7]: round(v) for k, v in t.items()}
         return d
 
     except FileNotFoundError:
         print(f"file not found")
         return None
-    
-def write_to_excel(d:dict)-> None:
-    df = pd.DataFrame(data = d, index=[0])
-    df.T.to_excel('electric_sum.xlsx', index_label='Month',header=['Kwh'])
-    
+
+
+def write_to_excel(d: dict) -> None:
+    df = pd.DataFrame(data=d, index=[0])
+    df.T.to_excel('electric_sum.xlsx', index_label='Month', header=['Kwh'])
 
 
 if __name__ == "__main__":
